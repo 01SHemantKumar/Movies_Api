3b2703117528eeb16f7c71ab2ca878c256988e5c
S.HEMANT KUMAR <sunny.hemant.kumar96@gmail.com>
2024-02-27 13:36:50 +0530

    added first mongo repo

---
+++ src/main/java/com/HSproject/Movies_Api/MoviesApiApplication.java
@@ -0,0 +1,13 @@
+package com.HSproject.Movies_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiApplication.class, args);
+	}
+
+}
